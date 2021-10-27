2bf268eac71da1d95dfebd95aa1faa2a54434fb8
HugoMage <69723114+HugoMage@users.noreply.github.com>
2021-10-26 21:20:24 -0500

    Update ItemsRegistry.java

--- main/java/com/hugomage/doyoubelieve/registerys/ItemsRegistry.java
+++ main/java/com/hugomage/doyoubelieve/registerys/ItemsRegistry.java
@@ -30,7 +30,7 @@ public class ItemsRegistry {
     // SpawnEgg
     public static final RegistryObject<ModSpawnEggItem> BIGFOOT_SPAWN_EGG = ITEMS.register("bigfoot_spawn_egg",() -> new ModSpawnEggItem(ModEntityTypes.BIGFOOT, 0x702817, 0x5E4944 , new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
     public static final RegistryObject<ModSpawnEggItem> JERSEY_DEVIL_SPAWN_EGG = ITEMS.register("jersey_devil_spawn_egg",() -> new ModSpawnEggItem(ModEntityTypes.JERSEY_DEVIL, 0x702817, 0x5E4944 , new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
-    public static final RegistryObject<ModSpawnEggItem> FRESNO_NIGHTCRAWLER_SPAWN_EGG = ITEMS.register("fresno_nightcrawler_spawn_egg",() -> new ModSpawnEggItem(ModEntityTypes.FRESNO, 0x702817, 0x5E4944 , new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<ModSpawnEggItem> FRESNO_NIGHTCRAWLER_SPAWN_EGG = ITEMS.register("fresno_nightcrawler_spawn_egg",() -> new ModSpawnEggItem(ModEntityTypes.FRESNO, 0x802817, 0x5E4944 , new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
 
     //Skulls
 
